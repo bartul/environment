954f784126deb8de957ac3e823ec73ddd1d252d8
Bartul Bonacic <bartul.bonacic@live.com>
2017-02-22 16:15:34 +0100

    Install vs code extenstions via script

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,24 @@
+code --install-extension ms-vscode.csharp
+code --install-extension cake-build.cake-vscode
+code --install-extension Ionide.Ionide-FAKE
+code --install-extension Ionide.Ionide-fsharp
+code --install-extension Ionide.Ionide-Paket
+code --install-extension DotJoshJohnson.xml
+code --install-extension ms-vscode.PowerShell
+code --install-extension sbrink.elm
+code --install-extension abadi199.elm-format
+
+code --install-extension dbaeumer.vscode-eslint
+
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension bbenoist.vagrant
+code --install-extension msjsdiag.debugger-for-chrome
+
+code --install-extension humao.rest-client
+code --install-extension dkundel.vscode-new-file
+
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension seanmcbreen.Spell
+code --install-extension EditorConfig.EditorConfig
+
+
