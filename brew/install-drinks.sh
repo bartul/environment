276b857404f1077f589cbaade1c680927ca5175f
Bartul Bonacic <bartul.bonacic@live.com>
2017-02-23 11:10:41 +0100

    Install brews via script

---
+++ brew/install-drinks.sh
@@ -0,0 +1,19 @@
+brew install tree
+brew install watch
+brew install trash
+brew install openssl
+
+brew install vim
+brew install curl
+brew install git
+brew install diff-so-fancy
+
+brew install mono
+brew install paket
+brew install elixir
+brew install elm
+brew install node
+brew install yarn
+
+
+brew install nyancat
