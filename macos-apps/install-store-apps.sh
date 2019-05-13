dfcef4ea813e6d76f46ff2433f9fb337594ec43d
Bartul Bonacic <bartul.bonacic@live.com>
2019-05-13 13:09:57 +0200

    update mac app store

--- macos-apps/install-store-apps.sh
+++ macos-apps/install-store-apps.sh
@@ -1,12 +1,16 @@
 mas install 715768417 # Microsoft Remote Desktop
 mas install 823766827 # OneDrive
-mas install 1071663619 # UnRAR Unarchiver - Zip, Rar Extractor,Expander
-mas install 503039729 # Fotor Photo Editor
 
 mas install 1147396723 # WhatsApp Desktop
 mas install 803453959 # Slack
 
 mas install 568494494 # Pocket
-mas install 865500966 # feedly
+mas install 865500966 # Feedly
 mas install 405399194 # Kindle
 
+# Office
+784801555 # OneNote 
+462058435 # Excel
+462062816 # Powerpoint
+985367838 # Outlook
+462054704 # Word
