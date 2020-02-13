a028204f0e6ca58b3078025c50aeadd9ec54e87f
Bartul Bonacic <bartul.bonacic@live.com>
2020-02-13 13:44:24 +0100

    Add proper elm in vscode

--- vscode/install-extensions.bat
+++ vscode/install-extensions.bat
@@ -5,20 +5,18 @@ code --install-extension Ionide.Ionide-fsharp
 code --install-extension Ionide.Ionide-Paket
 code --install-extension DotJoshJohnson.xml
 code --install-extension ms-vscode.PowerShell
-code --install-extension sbrink.elm
-code --install-extension abadi199.elm-format
+code --install-extension elmTooling.elm-ls-vscode
 
 code --install-extension dbaeumer.vscode-eslint
 
 code --install-extension PeterJausovec.vscode-docker
-code --install-extension bbenoist.vagrant
 code --install-extension msjsdiag.debugger-for-chrome
 
 code --install-extension humao.rest-client
 code --install-extension dkundel.vscode-new-file
 
 code --install-extension robertohuertasm.vscode-icons
-code --install-extension seanmcbreen.Spell
+code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension EditorConfig.EditorConfig
 
 
--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -5,13 +5,11 @@ code --install-extension Ionide.Ionide-fsharp
 code --install-extension Ionide.Ionide-Paket
 code --install-extension DotJoshJohnson.xml
 code --install-extension ms-vscode.PowerShell
-code --install-extension sbrink.elm
-code --install-extension abadi199.elm-format
+code --install-extension elmTooling.elm-ls-vscode
 
 code --install-extension dbaeumer.vscode-eslint
 
 code --install-extension PeterJausovec.vscode-docker
-code --install-extension bbenoist.vagrant
 code --install-extension msjsdiag.debugger-for-chrome
 
 code --install-extension humao.rest-client
