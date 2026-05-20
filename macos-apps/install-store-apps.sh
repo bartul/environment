c0319f6a9357b1e2c14467251eddb8e32e79c8eb
Bartul Bonacic <bartul.bonacic@live.com>
2026-05-20 22:13:15 +0200

    Sync macOS app install script with installed apps

--- macos-apps/install-store-apps.sh
+++ macos-apps/install-store-apps.sh
@@ -1,17 +1,16 @@
-mas install 1295203466 # Microsoft Remote Desktop
 mas install 823766827 # OneDrive
 
-mas install 1147396723 # WhatsApp Desktop
-mas install 803453959 # Slack
-
 mas install 568494494 # Pocket
-mas install 865500966 # Feedly
-mas install 405399194 # Kindle
 mas install 6760943472 # Read.md
 
+# Safari extensions
+mas install 1622835804 # Kagi for Safari
+mas install 1480933944 # Vimari
+
 # Office
-mas install 784801555 # OneNote 
+mas install 784801555 # OneNote
 mas install 462058435 # Excel
 mas install 462062816 # Powerpoint
 mas install 985367838 # Outlook
 mas install 462054704 # Word
+mas install 1274495053 # Microsoft To Do
