67d402101afbf1b733f2a4de70ed141803901175
Bartul Bonacic <bartul.bonacic@live.com>
2026-05-26 08:37:13 +0200

    Reconcile fish config
    Add dotnet tools to PATH using fish_add_path; move starship init inside
    is-interactive block; add vi key bindings.

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 set -x PATH $PATH /usr/local/bin/
 
-# Add dotnet tools
-export PATH="$PATH:$HOME/.dotnet/tools"
+fish_add_path $HOME/.dotnet/tools
 
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
