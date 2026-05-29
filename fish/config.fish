8ab834a3f2141a2ecaf471418c77f2f22cc56c21
Bartul Bonacic <bartul.bonacic@live.com>
2026-05-29 14:10:24 +0200

    Sync fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
-set -x PATH $PATH /usr/local/bin/
-
+# Prioritize Homebrew binaries over system binaries
+fish_add_path -p /opt/homebrew/bin
 fish_add_path $HOME/.dotnet/tools
 
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
+    bind -M insert ctrl-r history-pager
     starship init fish | source
 end
